f16886fae090be7ececa7ea017b8afef9dc9dbdf
pratyush48 <pratyushnandi48@gmail.com>
2020-05-16 11:58:30 +0530

    Made chnges in issue unit

--- issue.v
+++ issue.v
@@ -9,61 +9,141 @@ reg [15:0] L12_A, L12_B, L12_Z, L34_Z;
 reg [3:0] L12_rd, L12_func, L23_rd;
 reg [7:0] L12_addr, L23_addr, L34_addr;
 
-reg [15:0] regbank [0:15];
-reg [15:0] mem[0:255];
+wire rs1_b,rs2_b;
 always @(posedge clk1)
 begin
-    if (tomosulo.tail_p - tomosulo.head_p == 7)
-        if (tomosulo.tail_p == 7)
-            if(tomosulo.add_count < 3)
-                //call rs
-                //update the ROB
-                tomosulo.tail_p = 0;
-            else if(tomosulo.mul_count < 3)
-                //call rs
-                tomosulo.tail_p = 0;
-            else if(tomosulo.bch_count < 3)
-                //call rs
-                tomosulo.tail_p = 0;
-            else
-                //stall
-
-    else if(tomosulo.tail_p - tomosulo.head_p < 7)
-        if (tomosulo.tail_p == 7)
-            if(tomosulo.add_count < 3)
-                //call rs
-                tomosulo.tail_p = 0;
-            else if(tomosulo.mul_count < 3)
-                //call rs
-                tomosulo.tail_p = 0;
-            else if(tomosulo.bch_count < 3)
-                //call rs
-                tomosulo.tail_p = 0;
-            else
-                //stall
-        else
-            if(tomosulo.add_count < 3)
-                //call rs
-                tomosulo.tail_p += 1;
-            else if(tomosulo.mul_count < 3)
-                //call rs
-                tomosulo.tail_p += 1;
-            else if(tomosulo.bch_count < 3)
-                //call rs
-                tomosulo.tail_p += 1;
-            else
-                //stall
+    if(tomasulo.regbank[rs1][1] < 8)
+    begin
+        rs1_b = 0;
+        rs1 = tomasulo.regbank[rs1][1];  
+    end
+    else 
+        rs1_b = 1;
+    if(tomasulo.regbank[rs2][1] < 8)
+    begin
+        rs2_b = 0;
+        rs2 = tomasulo.regbank[rs2][1];  
+    end
     else
-        if(tomosulo.add_count < 3)
+        rs2_b = 1;
+    if (tomasulo.tail_p - tomasulo.head_p == 7)
+    begin
+        if (tomasulo.tail_p == 7)
+        begin
+            if(tomasulo.add_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd;    
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;            
+                tomasulo.tail_p <= 0;
+            end
+            else if(tomasulo.mul_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd;    
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                             
+                tomasulo.tail_p <= 0;
+            end
+            else if(tomasulo.bch_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd; 
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                                
+                tomasulo.tail_p <= 0;
+            end
+            else
+                //stall
+        end
+    end
+    else if(tomasulo.tail_p - tomasulo.head_p < 7)
+    begin
+        if (tomasulo.tail_p == 7)
+        begin
+            if(tomasulo.add_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd; 
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                                
+                tomasulo.tail_p <= 0;
+            end
+            else if(tomasulo.mul_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd;
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                                 
+                tomasulo.tail_p <= 0;
+            end
+            else if(tomasulo.bch_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd; 
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                                
+                tomasulo.tail_p <= 0;
+            end
+            else
+                //stall
+        end
+        else
+        begin
+            if(tomasulo.add_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd;
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                                 
+                tomasulo.tail_p <= tomasulo.tail_p + 1;
+            end
+            else if(tomasulo.mul_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd; 
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                                
+                tomasulo.tail_p <= tomasulo.tail_p + 1;
+            end
+            else if(tomasulo.bch_count < 3)
+            begin
+                //call rs
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd; 
+                tomasulo.regbank[rd][1] <= tomasulo.tail_p;                                
+                tomasulo.tail_p <= tomasulo.tail_p + 1;
+            end
+            else
+                //stall
+        end
+    end
+    else
+    begin
+        if(tomasulo.add_count < 3)
+        begin    
             //call rs
-            tomosulo.tail_p += 1;
-        else if(tomosulo.mul_count < 3)
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd;             
+                tomasulo.tail_p <= tomasulo.tail_p + 1;
+        end
+        else if(tomasulo.mul_count < 3)
+        begin
             //call rs
-            tomosulo.tail_p += 1;
-        else if(tomosulo.bch_count < 3)
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd;             
+                tomasulo.tail_p <= tomasulo.tail_p + 1;
+        end
+        else if(tomasulo.bch_count < 3)
+        begin
             //call rs
-            tomosulo.tail_p += 1;
+                tomasulo.ROB[tomasulo.tail_p][0] <= func;
+                tomasulo.ROB[tomasulo.tail_p][1] <= rd;             
+                tomasulo.tail_p <= tomasulo.tail_p + 1;
+        end
         else
             //stall
+    end
 end
 assign Zout = L34_Z;
